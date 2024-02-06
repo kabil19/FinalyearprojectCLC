1415b850ce5beacbbbd7b446d7e2591e4cac42d3
kabil19 <kabilankrishna5@gmail.com>
2024-02-06 20:33:44 +0530

    tempInvoice Response Entity[06/02/2024]

--- CLC/BackEnd/CLC-Api/CLC-Api/src/main/java/com/appli/clcapi/tempInvoice/controller/TempInvoiceController.java
+++ CLC/BackEnd/CLC-Api/CLC-Api/src/main/java/com/appli/clcapi/tempInvoice/controller/TempInvoiceController.java
@@ -4,7 +4,6 @@ package com.appli.clcapi.tempInvoice.controller;
 import com.appli.clcapi.tempInvoice.dto.TempInvoiceDto;
 import com.appli.clcapi.tempInvoice.service.TempInvoiceService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
